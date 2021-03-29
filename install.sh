9a3473de6e8c0ab8779c95cbbb0940d6916afe1b
matej@megabedna-bionic <matej.petrlik@fel.cvut.cz>
2021-03-29 13:17:29 +0200

    added louiseville mega cavern

--- install.sh
+++ install.sh
@@ -4,17 +4,21 @@ MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
 FILENAME_PILSEN_TESTBED=pilsen_robot_testbed_ign.tar.gz
+FILENAME_LOUISEVILLE=louiseville_mega_cavern_ign.tar.gz
 
 [ ! -d $MY_PATH/models ] && mkdir $MY_PATH/models
 cd $MY_PATH/models
 
 echo "Downloading models from NAS"
 wget --no-check-certificate -O $FILENAME_PILSEN_TESTBED https://nasmrs.felk.cvut.cz/index.php/s/6fFN8GhhstoSkLA/download
+wget --no-check-certificate -O $FILENAME_LOUISEVILLE https://nasmrs.felk.cvut.cz/index.php/s/fE1sNg0IqeF4v6s/download
+
 
 echo "Unzipping models"
 tar -xvzf $FILENAME_PILSEN_TESTBED
+tar -xvzf $FILENAME_LOUISEVILLE
 
 echo "Cleaning up"
-rm $FILENAME_CAVE $FILENAME_URBAN $FILENAME_PILSEN_TESTBED
+rm $FILENAME_PILSEN_TESTBED $FILENAME_LOUISEVILLE
 
 echo "Done"
